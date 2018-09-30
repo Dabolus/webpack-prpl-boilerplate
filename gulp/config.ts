9652479ae32ad7d1f0a1ad9cb7e3395bd0ed883d
Giorgio Garasto <giorgio@garasto.it>
2018-09-30 20:35:05 +0200

    feature: Let gulp generate PRPL config file

--- gulp/config.ts
+++ gulp/config.ts
@@ -1,6 +1,8 @@
-/* tslint:disable:no-implicit-dependencies */
+/* tslint:disable:no-implicit-dependencies object-literal-sort-keys */
 import { exec } from 'child_process';
-import { parallel, task } from 'gulp';
+import { writeFile } from 'fs';
+import { parallel, series, task } from 'gulp';
+import { resolve } from 'path';
 
 task('build:es5', (cb) =>
   exec('BUILD_NAME="es5" BROWSERSLIST="ie > 9" npm run build:static', cb));
@@ -8,4 +10,17 @@ task('build:es5', (cb) =>
 task('build:es6', (cb) =>
   exec('BUILD_NAME="es6" BROWSERSLIST="edge > 12" npm run build:static', cb));
 
-task('build', parallel('build:es5', 'build:es6'));
+task('write-polymer-config', (cb) =>
+  writeFile(resolve(__dirname, '../build/polymer.json'), JSON.stringify({
+    entrypoint: 'index.html',
+    builds: [{
+      name: 'es6',
+      browserCapabilities: [
+        'es2015',
+      ],
+    }, {
+      name: 'es5',
+    }],
+  }, null, 2), cb));
+
+task('build', series(parallel('build:es5', 'build:es6'), 'write-polymer-config'));
