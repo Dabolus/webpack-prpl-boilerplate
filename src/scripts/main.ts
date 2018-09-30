fd4446bae0e97e01051bae92ea8d782eff728c4a
Giorgio Garasto <giorgio@garasto.it>
2018-09-30 22:45:29 +0200

    feature: Make the website more easy to get started with

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,18 @@
 // This shall be the starting point for your code
+const counter = document.querySelector<HTMLPreElement>('#clock');
+
+const addZero = (n: number): string => n > 9 ? n.toString() : `0${n}`;
+
+const formatDate = (date: Date): string => {
+  const h = date.getHours();
+  const m = date.getMinutes();
+  const s = date.getSeconds();
+  return `${addZero(h)}:${addZero(m)}:${addZero(s)}`;
+};
+
+const updateClock = () =>
+  requestAnimationFrame(() =>
+    counter.textContent = formatDate(new Date()));
+
+setInterval(updateClock, 500);
+updateClock();
