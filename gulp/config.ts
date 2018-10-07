c9a82eb0e6fe1584fc1accd29246c0739a51c4f4
Giorgio Garasto <giorgio@garasto.it>
2018-10-07 15:40:17 +0200

    refactor: Make it even more simpler to add new builds

--- gulp/config.ts
+++ gulp/config.ts
@@ -2,26 +2,16 @@
 import { writeFile } from 'fs';
 import { parallel, series, task } from 'gulp';
 import { resolve as resolvePath } from 'path';
-import { build } from './helpers';
+import { build, polymerConfig } from './helpers';
 
 task('build:es5', () =>
   build('es5', 'ie > 9'));
 
 task('build:es6', () =>
-  build('es6', 'edge > 12'));
+  build('es6', 'edge > 12', ['es2015']));
 
 task('write-polymer-config', (cb) =>
-  writeFile(resolvePath(__dirname, '../build/polymer.json'), `${JSON.stringify({
-    entrypoint: 'index.html',
-    builds: [{
-      name: 'es6',
-      browserCapabilities: [
-        'es2015',
-      ],
-    }, {
-      name: 'es5',
-    }],
-  }, null, 2)}\n`, cb));
+  writeFile(resolvePath(__dirname, '../build/polymer.json'), polymerConfig(), cb));
 
 task('build', series(
   parallel('build:es5', 'build:es6'),
--- gulp/helpers.ts
+++ gulp/helpers.ts
@@ -1,11 +1,33 @@
+/* tslint:disable:no-implicit-dependencies object-literal-sort-keys */
 import { exec } from 'child_process';
 
-export const build = (buildName: string, browsersList: string) =>
+interface IPRPLConfigBuild {
+  name: string;
+  browserCapabilities?: string[];
+}
+
+interface IPRPLConfig {
+  entrypoint: string;
+  builds: IPRPLConfigBuild[];
+}
+
+const config: IPRPLConfig = {
+  entrypoint: 'index.html',
+  builds: [],
+};
+
+export const build = (buildName: string, browsersList: string, browserCapabilities?: string[]) =>
   new Promise((resolve, reject) =>
     exec(`BUILD_NAME=${buildName} BROWSERSLIST="${browsersList}" npm run build:static`, (err) => {
       if (err) {
         reject(err);
       } else {
+        config.builds.push({
+          name: buildName,
+          ...browserCapabilities ? { browserCapabilities } : {},
+        });
         resolve();
       }
     }));
+
+export const polymerConfig = () => `${JSON.stringify(config, null, 2)}\n`;
